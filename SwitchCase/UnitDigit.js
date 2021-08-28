a503a5f15fae539e588568716e7adc0e4f14548d
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-08-28 12:28:37 +0530

    [Ariprasath] added: practice problems using Switch case. problems added: numbers to words, numbers to week day names, finding the unit digits, unit conversions.

---
+++ SwitchCase/UnitDigit.js
@@ -0,0 +1,25 @@
+var num=1000;
+switch(num)
+{
+    case 1:
+        console.log("One");
+        break;
+    case 10:
+        console.log("Ten");
+        break;
+    case 100:
+        console.log("Hundred");
+        break;
+    case 1000:
+        console.log("Thousand");
+        break;
+    case 10000:
+        console.log("Ten thousand");
+        break;
+    case 100000:
+        console.log("1 Lakh");
+        break;
+    default:
+        console.log("enter correct number");
+        break;
+}
